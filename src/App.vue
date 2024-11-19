943b380b83dc2a4f05d4f959bc23d9c7f92662f9
bonnieace <bonniemasota@gmail.com>
2024-11-19 12:53:23 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,55 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <h1>To-Do List</h1>
+    <form @submit.prevent="addTask">
+      <input v-model="newTask" placeholder="Enter a task" />
+      <button type="submit">Add Task</button>
+    </form>
+    <ul>
+      <li 
+        v-for="(task, index) in tasks" 
+        :key="index" 
+        :class="{ completed: task.completed }"
+      >
+        <input type="checkbox" v-model="task.completed" />
+        {{ task.name }}
+        <button @click="removeTask(index)">Remove</button>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      newTask: '',
+      tasks: [],
+    };
+  },
+  methods: {
+    addTask() {
+      if (this.newTask.trim()) {
+        this.tasks.push({ name: this.newTask, completed: false });
+        this.newTask = '';
+      }
+    },
+    removeTask(index) {
+      this.tasks.splice(index, 1);
+    },
+  },
+};
 </script>
 
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 50px;
+}
+
+.completed {
+  text-decoration: line-through;
+  color: gray;
 }
 </style>
